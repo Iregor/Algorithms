2f60866d873745b77639b650d81634c129a361e9
Igor <mamontovIS@yandex.ru>
2023-08-26 15:01:17 +0300

    Solved problem "Sqrt" from binSearch (upgrade description).

--- src/SqrtX.java
+++ src/SqrtX.java
@@ -1,3 +1,19 @@
+/*
+LeetCode 69. Sqrt(x)
+Easy
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.*/
+
 public class SqrtX {
     public static void main(String[] args) {
         Solution sol = new SqrtX().new Solution();
